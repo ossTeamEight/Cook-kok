3d83ebb63486ab48164e40f089cdab7c97f15fcb
Jin Lyu <bme201601103@hufs-gsuite.kr>
2021-05-10 17:59:05 +0900

    list Activity update

--- app/src/main/java/org/techtown/cook_kok/listActivity.java
+++ app/src/main/java/org/techtown/cook_kok/listActivity.java
@@ -1,7 +1,8 @@
 package org.techtown.cook_kok;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class listActivity extends AppCompatActivity {
@@ -11,5 +12,30 @@ public class listActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
     }
+
+    public void onClick01(View v) {
+        /*Intent intent01 = new Intent(Intent.ACTION_VIEW, );
+        startActivity(intent01);*/
+        Toast.makeText(getApplicationContext(),"한식list",Toast.LENGTH_LONG).show();
+    }
+    public void onClick02(View v) {
+        /*Intent intent02 = new Intent(Intent.ACTION_VIEW,);
+        startActivity(intent02);*/
+        Toast.makeText(getApplicationContext(),"중식list",Toast.LENGTH_LONG).show();
+    }
+    public void onClick03(View v) {
+        /*Intent intent03 = new Intent(Intent.ACTION_VIEW,);
+        startActivity(intent03);*/
+        Toast.makeText(getApplicationContext(),"일식list",Toast.LENGTH_LONG).show();
+    }
+    public void onClick04(View v) {
+        /*Intent intent04 = new Intent(Intent.ACTION_VIEW,);
+        startActivity(intent04);*/
+        Toast.makeText(getApplicationContext(),"양식list",Toast.LENGTH_LONG).show();
+    }
+    public void onClick05(View v) {
+        /*Intent intent05 = new Intent(Intent.ACTION_VIEW,);
+        startActivity(intent05);*/
+        Toast.makeText(getApplicationContext(),"기타list",Toast.LENGTH_LONG).show();
+    }
 }
-## 이번에는 어떻게든 될 수 있지 않을까요..ㅠ
