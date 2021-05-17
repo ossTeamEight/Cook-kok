fd66caa023eab4ec32222f54d1dfa74d6a86f7db
Yeolee Park <qkrdufdl358@naver.com>
2021-05-18 00:49:15 +0900

    recommendActivity_버튼의 intent생성

--- app/src/main/java/org/techtown/cook_kok/recommendActivity.java
+++ app/src/main/java/org/techtown/cook_kok/recommendActivity.java
@@ -2,13 +2,48 @@ package org.techtown.cook_kok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class recommendActivity extends AppCompatActivity {
+
+    Button btn_no_text;
+    Button btn_yes_text;
+    Button btn_dontNo_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+
+
+        btn_no_text = findViewById(R.id.btn_no_text);
+        btn_no_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(recommendActivity.this, recommendActivity1.class);
+                startActivities(intent); //액티비티 이동.
+            }
+        });
+
+        btn_yes_text = findViewById(R.id.btn_yes_text);
+        btn_yes_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(recommendActivity.this, recommendActivity1.class);
+                startActivities(intent); //액티비티 이동.
+            }
+        });
+
+        btn_dontNo_text = findViewById(R.id.btn_dontNo_text);
+        btn_dontNo_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(recommendActivity.this, recommendActivity1.class);
+                startActivities(intent); //액티비티 이동.
+            }
+        });
     }
 }
