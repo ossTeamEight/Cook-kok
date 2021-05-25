858f29db314efbe5eb90a395042b9d46169e60f5
Yeolee Park <qkrdufdl358@naver.com>
2021-05-25 14:46:37 +0900

    엑티비티 3개 추가, 버튼 1개 제거

--- app/src/main/java/org/techtown/cook_kok/recommendActivity2.java
+++ app/src/main/java/org/techtown/cook_kok/recommendActivity2.java
@@ -2,7 +2,6 @@ package org.techtown.cook_kok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.content.Intent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +11,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-public class recommendActivity extends AppCompatActivity {
+public class recommendActivity2 extends AppCompatActivity {
 
     Button btn_no_text;
     Button btn_yes_text;
@@ -22,14 +21,13 @@ public class recommendActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommend);
-
+        setContentView(R.layout.activity_recommend2);
 
         btn_no_text = findViewById(R.id.btn_no_text);
         btn_no_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(recommendActivity.this,recommendActivity2.class);
+                Intent intent = new Intent(recommendActivity2.this,recommendActivity3.class);
                 startActivity(intent); //액티비티 이동.
             }
         });
@@ -38,7 +36,7 @@ public class recommendActivity extends AppCompatActivity {
         btn_yes_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(recommendActivity.this,recommendActivity2.class);
+                Intent intent = new Intent(recommendActivity2.this,recommendActivity3.class);
                 startActivity(intent); //액티비티 이동.
             }
         });
@@ -50,7 +48,7 @@ public class recommendActivity extends AppCompatActivity {
         sprit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"저는 누르는게 아니에요!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"저는 누르는게 아니라구요!",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -61,6 +59,5 @@ public class recommendActivity extends AppCompatActivity {
 
             }
         });
-
     }
 }
