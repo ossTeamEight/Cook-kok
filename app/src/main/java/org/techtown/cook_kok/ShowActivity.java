a6a033b8e6a3434e4eaac59c82f44158e6bf62d9
Jungwoo Choi <choijungwoo493>
2021-06-04 20:13:03 +0900

    툴바네비게이션 아이콘 뺌

--- app/src/main/java/org/techtown/cook_kok/ShowActivity.java
+++ app/src/main/java/org/techtown/cook_kok/ShowActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class showActivity extends AppCompatActivity {
+public class ShowActivity extends AppCompatActivity {
     Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
