7e35fa1f3c09bd8d62396996c699c613f46ad43e
jdhyun09 <jdhyun09@naver.com>
2021-06-08 15:58:12 +0900

    돌림판 입력받기 액티비티 및 코드 추가

--- app/src/main/java/org/techtown/cook_kok/Put_nameActivity.java
+++ app/src/main/java/org/techtown/cook_kok/Put_nameActivity.java
@@ -2,13 +2,69 @@ package org.techtown.cook_kok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Put_nameActivity extends AppCompatActivity {
 
+    Boolean check = false;
+    String food_names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_putname);
+
+        Button goRoulette = findViewById(R.id.goRotate);
+        TextInputLayout inputLayout = findViewById(R.id.input_layout);
+
+        EditText editText = inputLayout.getEditText();
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String pattern = "^[\\w,]+,$";
+                if(!Pattern.matches(pattern, s)){
+                    inputLayout.setError("형식에 맞춰주세요.");
+                    check = false;
+                }else{
+                    inputLayout.setError(null);
+                    check = true;
+                    food_names = s.toString();
+                }
+            }
+        });
+
+        goRoulette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (check) {
+                    Intent intent_roulette = new Intent(Put_nameActivity.this, RouletteActivity.class);
+                    intent_roulette.putExtra("food_names", food_names);
+                    startActivity(intent_roulette);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"입력을 완성해주세요!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
